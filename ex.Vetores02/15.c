737b8b385414a096625e9d94bbf522e1d41be922
AlineOP201 <alineveira2@gmail.com>
2023-10-04 10:10:13 -0300

    10h10

---
+++ ex.Vetores02/15.c
@@ -0,0 +1,37 @@
+/*
+
+*/
+#include <stdio.h>
+
+int main () {
+//  Variaveis
+    int vetor[10], igual;
+
+//  Inserir informações no vetor
+    for (int i = 0; i < 10; i++) {
+        printf("Inserir valor: ");
+        scanf("%d", &vetor[i]);
+    }
+
+    for (int i = 0; i < 10; i++) {
+        igual = 0;
+        for (int j = i + 1; i < 10; j++) {
+            if (vetor[i] == vetor[j]) {
+                igual = 1;
+            }
+        }
+        if (igual != 1) {
+            printf("Somente valores distintos: %d", vetor[1]);
+        }
+        
+    }
+    
+
+
+
+
+
+
+
+    return 0;
+}
