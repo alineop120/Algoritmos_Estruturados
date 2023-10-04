68241b0a781121de75ce2307ea207e16a5b9d938
AlineOP201 <alineveira2@gmail.com>
2023-10-04 09:57:47 -0300

    09h57

---
+++ ex.Vetores02/14.c
@@ -0,0 +1,27 @@
+/*
+    Faça um programa que leia um vetor de 10 posições e verifique se existem valores iguais e os escreva na tela. 
+*/
+#include <stdio.h>
+
+int main () {
+//  Variaveis
+    int vetor[10], igual;
+
+//  Inserir informações no vetor
+    for (int i = 0; i < 10; i++) {
+        printf("Inserir valor: ");
+        scanf("%d", &vetor[i]);
+    }
+
+    for (int i = 0; i < 10; i++) {
+//  Percorrer proximas posições para buscar valores iguais
+        for (int j = i + 1; j < 10; j++) {
+            if () {
+                
+            }
+            
+        }
+    }
+
+    return 0;
+}
