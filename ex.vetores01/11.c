aec13494262147ed6719c194f9cf2d617a69c23f
AlineOP201 <alineveira2@gmail.com>
2023-10-04 11:06:52 -0300

    11h06

--- ex.vetores01/11.c
+++ ex.vetores01/11.c
@@ -3,21 +3,25 @@
 */
 #include <stdio.h>
 
-int main () {
+int main() {
 
     float valores[10], soma_positivos = 0, qtd_negativos = 0;
 
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 10; i++)
+    {
         printf("Inserir as notas: ");
         scanf("%f", &valores[i]);
     }
-    
-    for (int i = 0; i < 10; i++) {
-        for(valores[i] > 0) {
-        soma_positivos += valores[i];
-        } else {
-            
-        }
-    }
 
+    for (int i = 0; i < 10; i++)
+    {
+        if (valores[i] > 0) {
+            soma_positivos += valores[i];
+        } else {
+            qtd_negativos++;
+        }
+        printf("A soma dos positivos eh %.2f e a quantidade de negativos eh %i\n", soma_positivos, qtd_negativos);
+
+        return 0;
+    }
 }
