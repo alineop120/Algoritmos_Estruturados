e23c2fde9b4db6d829d23b63564fa33764ab0741
AlineOP201 <alineveira2@gmail.com>
2023-10-28 10:50:14 -0300

    10h50

--- Exercícios/12 Funções_02/ex01.c
+++ Exercícios/12 Funções_02/ex01.c
@@ -9,24 +9,76 @@
 #include <stdio.h>
 #include <math.h>
 
+// Protótipo
+int opcao_pagamento();
+void preco_aVista();
+
+int main() 
+{
+    int opcao;
+    float total_compra;
+
+    printf("Digite o total da compra: ");
+    scanf("%f", &total_compra);
+    opcao = opcao_pagamento();
+
+    return 0;
+}
+
+int opcao_pagamento()
+{
+    int opcao_escolha;
+    do
+    {
+        printf("(1) - A vista - 10 porcento de desconto\n");
+        printf("(2) - Duas vezes - preço ds etiqueda\n");
+        printf("(3) - 3 a 10 vezes com juros de 3 porcento\n");
+        printf("Digite a opcao escolhida: ");
+        scanf("%d", &opcao_escolha);
+
+        if (opcao_escolha < 1 || opcao_escolha > 3)
+        {
+            printf("Opcao invalida");
+        }
+        
+    } while (opcao_escolha < 1 || opcao_escolha > 3);
+    
+    return opcao_escolha;
+}
+
+
+
+
+
+
+
+
+
+
+/*
 // Opções de Pagamento
-double a_vista(double preco)                    //a vista com 10% de desconto
+void a_vista(double preco)                    //a vista com 10% de desconto
 {
     //Desconto 10% = 0.10
     return preco * 0.10;
 }
 
-double preco_etiqueta(double preco)    //em duas vezes (preço da etiqueta)
+void preco_etiqueta(double preco)    //em duas vezes (preço da etiqueta)
 {
     //Desconto 10% = 0.10
     double preco_desconto = preco * 0.10;
     return preco + pow(preco_desconto,2);
 }
 
-double juros_mes()                              //de 3 até 10 vezes com 3% de juros ao mês (somente para compras acima de R$ 100,00)                   
+void juros_mes(double preco)                              //de 3 até 10 vezes com 3% de juros ao mês (somente para compras acima de R$ 100,00)                   
 {
-    //Fórmula - Juros Simples
+    if (preco < 100)
+    {
+        //Fórmula - Juros Simples
+        // JS (Juro Simples) = C (capital) * I (taxa de juro) * T (tempo) 
     return ;
+    }
+    
 }
 
 // Estrutura do código
@@ -37,7 +89,7 @@ int main()
 
     printf("Informe o valor da compra: ");
     scanf("%lf", &valor_compra);
-    printf("Informe a forma do pagamento: ");
+    printf("Informe a forma do pagamento - \n (1) a vista com 10% de desconto \n (2)em duas vezes (preço da etiqueta) \n (3) de 3 até 10 vezes com 3% de juros ao mês (somente para compras acima de R$ 100,00): ");
     scanf("%s", &opcao);
 
     // Opções de pagamento
@@ -66,3 +118,4 @@ int main()
 
     return 0;
 }
+*/
