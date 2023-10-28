49a04debea4ac79bda18fc08425d8b3aa6a75cf5
AlineOP201 <alineveira2@gmail.com>
2023-10-28 11:35:00 -0300

    11h34

--- Exercícios/12 Funções_02/ex02.c
+++ Exercícios/12 Funções_02/ex02.c
@@ -1,3 +1,16 @@
 /*
     Uma locadora de vídeos tem guardada, em um vetor A de 500 posições, a quantidade de filmes retirados por seus clientes durante o ano de 2020. Agora, esta locadora está fazendo uma promoção e, para cada 15 filmes retirados, o cliente tem direito a uma locação grátis. Faça um procedimento que receba o vetor A por parâmetro e retorna um vetor contendo a quantidade de locações gratuitas a que cada cliente tem direito. 
 */
+
+/*
+    trocar L2 com L8 (matriz[10][10])
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            int temp;
+            temp = matriz[i][i];
+            matriz[i][3] = matriz[i][9 - i];
+            matriz[i][9] = temp;
+        }
+    }
+*/
