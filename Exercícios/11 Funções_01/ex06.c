7e1827afc2bb24dd2fcc6c00e00a24b48446ec7f
AlineOP201 <alineveira2@gmail.com>
2023-10-28 09:31:10 -0300

    09h31

---
+++ Exercícios/11 Funções_01/ex06.c
@@ -0,0 +1,27 @@
+/*
+    Escreva uma função que receba o peso (quilos) e a altura (metros) de uma pessoa. Calcule e retorne o IMC (Índice de Massa Corporal) dessa pessoa:
+        IMC = peso / (altura * altura)
+*/
+#include <stdio.h>
+#include <math.h>
+
+float imc(float peso_kg, float altura_m)
+{
+    return peso_kg / pow(altura_m,2);
+}
+
+int main()
+{
+    float peso, altura, resultado;
+
+    printf("Informe o peso (quilos): ");
+    scanf("%f", &peso);
+    printf("Informe a altura (metros): ");
+    scanf("%f", &altura);
+
+    resultado = imc(peso, altura);
+
+    printf("O IMC da pessoa e ", resultado);
+
+    return 0;
+}
