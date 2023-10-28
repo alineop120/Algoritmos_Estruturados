e657d9e335760d65f872cf97dc0f44ebc8c68fd0
AlineOP201 <alineveira2@gmail.com>
2023-10-28 11:27:03 -0300

    11h26 adicionando arquivo

---
+++ Exercícios/12 Funções_02/ex02.c
@@ -0,0 +1,3 @@
+/*
+    Uma locadora de vídeos tem guardada, em um vetor A de 500 posições, a quantidade de filmes retirados por seus clientes durante o ano de 2020. Agora, esta locadora está fazendo uma promoção e, para cada 15 filmes retirados, o cliente tem direito a uma locação grátis. Faça um procedimento que receba o vetor A por parâmetro e retorna um vetor contendo a quantidade de locações gratuitas a que cada cliente tem direito. 
+*/
---
+++ Exercícios/12 Funções_02/ex03.c
@@ -0,0 +1,8 @@
+/*
+    Faça um procedimento que receba uma matriz A(10,10), por parâmetro, e realize as seguintes trocas:
+        (a) a linha 2 com a linha 8;
+        (b) a coluna 4 com a coluna 10;
+        (c) a diagonal principal com a secundária;
+        (d) a linha 5 com a coluna 10.
+    O procedimento deve retornar a matriz alterada.
+*/
