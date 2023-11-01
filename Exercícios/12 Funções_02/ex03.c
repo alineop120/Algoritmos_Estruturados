6f6c2b782b0b5e71b6b5b4de412ec7466b02072d
AlineOP201 <alineveira2@gmail.com>
2023-11-01 09:17:37 -0300

    09h17

--- Exercícios/12 Funções_02/ex03.c
+++ Exercícios/12 Funções_02/ex03.c
@@ -6,3 +6,16 @@
         (d) a linha 5 com a coluna 10.
     O procedimento deve retornar a matriz alterada.
 */
+
+/*
+    trocar L2 com L8 (matriz[10][10])
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            int temp;
+            temp = matriz[i][i];
+            matriz[i][3] = matriz[i][9 - i];
+            matriz[i][9] = temp;
+        }
+    }
+*/
