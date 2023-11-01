d91038fbe7381e5baa2c48448ede6a6d5fb1dc69
AlineOP201 <alineveira2@gmail.com>
2023-11-01 10:20:29 -0300

    10h20

--- Exercícios/12 Funções_02/ex03.c
+++ Exercícios/12 Funções_02/ex03.c
@@ -24,12 +24,50 @@ void imprimirMatriz(int matriz[10][10])
 void troca_linha2Linha8(int matriz[10][10])
 {
     int aux;
-
     for (int j = 0; j < 10; j++)
     {
         aux = matriz[1][j];
         matriz[1][j] = matriz[7][j];
         matriz[7][j] = aux;
+    }
+    
+}
+
+// (b) a coluna 4 com a coluna 10
+void troca_coluna4Coluna10(int matriz[10][10])
+{
+    int aux;
+    for (int i = 0; i < 10; i++)
+    {
+        aux = matriz[i][3];
+        matriz[i][3] = matriz[i][9];
+        matriz[i][9] = aux;
+    }
+    
+}
+
+// (c) a diagonal principal com a secundária
+void troca_diagonais(int matriz[10][10])
+{
+    int aux;
+    for (int i = 0; i < 10; i++)
+    {
+        aux = matriz[i][i];
+        matriz[i][i] = matriz[i][9 - i];
+        matriz[i][9 - i] = aux;
+    }
+    
+}
+
+// (d) a linha 5 com a coluna 10
+void troca_linha5Coluna10(int matriz[10][10])
+{
+    int aux;
+    for (int i = 0; i < 10; i++)
+    {
+        aux = matriz[i][3];
+        matriz[i][3] = matriz[i][9];
+        matriz[i][9] = aux;
     }
     
 }
@@ -46,8 +84,16 @@ int main()
         }
     }
 
-    imprimirMatriz(matriz);
+    printf("Matriz Original\n");
+    imprimirMatriz(matriz); //original
+    //troca_linha2Linha8(matriz);
+    //troca_coluna4Coluna10(matriz);
+    //troca_diagonais(matriz);
+    troca_linha5Coluna10(matriz);
 
+    imprimirMatriz(matriz);
+    
+    return 0;
 }
 /*
     trocar L2 com L8 (matriz[10][10])
