20400540df715e6e4c5b986c72068c5dab980c71
AlineOP201 <alineveira2@gmail.com>
2023-11-11 09:19:52 -0300

    09h19 adicionando código

--- Exercícios/10 Vetores_02/ex19.c
+++ Exercícios/10 Vetores_02/ex19.c
@@ -1,3 +1,22 @@
 /*
     Faça um vetor de tamanha 50 preenchido com o seguinte valor: (i + 5*i) % (i + 1), sendo i a posição do elemnto no vetor. Em seguida imprima o vetor na tela.
 */
+#include <stdio.h>
+
+int main()
+{
+    int tamanho = 50;
+    int preencher_vetor[tamanho];
+
+    for (int i = 0; i < tamanho; i++)
+    {
+        preencher_vetor[i] = (i + 5*i) % (i + 1);
+    }
+
+    for (int i = 0; i < tamanho; i++)
+    {
+        printf("%d -> %d", i, preencher_vetor[i]);
+    }
+    
+    
+}
