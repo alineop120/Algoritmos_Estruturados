29e12374611954570d055f82dddc6ec6a9478d80
AlineOP201 <alineveira2@gmail.com>
2023-11-11 09:32:00 -0300

    09h31 adicionando enunciado do exercicio

---
+++ Exercícios/10 Vetores_02/ex20.c
@@ -0,0 +1,3 @@
+/*
+    Escreva um programa que leia números inteiros no intervalo [0,50] e os armazene em um vetor com 10 posições. Preencha um segundo vetor apenas com os números ímapares do primeiro vetor. imprima os dois vetores, 2 elementos por linha.
+*/
