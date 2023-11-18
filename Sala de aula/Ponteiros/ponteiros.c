40eb930c7a0f33a839fbb391ab63e46f30f4b25e
AlineOP201 <alineveira2@gmail.com>
2023-11-18 09:43:08 -0300

    09h43

--- Sala de aula/Ponteiros/ponteiros.c
+++ Sala de aula/Ponteiros/ponteiros.c
@@ -1,4 +1,18 @@
 /*
     Ponteiros
         Variável que armazena o endereço de outra variável.
+    Variável
+        Armazena dados.
 */
+#include <stdio.h>
+
+int main () {
+// Variável
+    int numero;
+    int *ponteiro;
+
+// Entrada de Dados
+    printf("O endereco da variavel 'numero': %d", &numero);
+
+    return 0;
+}
