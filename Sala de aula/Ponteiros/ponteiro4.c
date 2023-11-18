42a293e553413c278f7f4c50450a4c8a2b6a5ad3
AlineOP201 <alineveira2@gmail.com>
2023-11-18 10:51:27 -0300

    10h51

--- Sala de aula/Ponteiros/ponteiro4.c
+++ Sala de aula/Ponteiros/ponteiro4.c
@@ -1,17 +1,18 @@
 // Função para trocar o valor da variavel
 #include <stdio.h>
 
-void mudaValor(int num)
+void mudaValor(int *num)
 {
-    num = 30;
+    *num = 30;
 }
 
 int main () 
 {
-    
     int numero;
 
-    mudaValor(numero);
+    mudaValor(&numero);
+
+    printf("Valor da variavel numero: %d", numero);
 
     return 0;
 }
