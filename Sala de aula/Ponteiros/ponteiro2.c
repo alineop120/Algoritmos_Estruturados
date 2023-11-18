d01edc6af9127a7c7ccaef6f5a5e4563351ee81c
AlineOP201 <alineveira2@gmail.com>
2023-11-18 10:34:52 -0300

    10h34

--- Sala de aula/Ponteiros/ponteiro2.c
+++ Sala de aula/Ponteiros/ponteiro2.c
@@ -3,16 +3,21 @@
 int main () 
 {
     int numero = 20;
-    int *ponteiro;
+    int *ponteiro = NULL;
+    int *ponteiro2;
+
+    printf("\nO conteudo do numero (antes das atribuicao): %p", ponteiro);
 
     ponteiro = &numero;
+    ponteiro2 = &numero;
 
-    printf("O endereco de memoria do numero (atraves do ponteiro): %d", *ponteiro);
+    printf("\nO endereco de memoria do numero (atraves do ponteiro): %d", *ponteiro);
     printf("\nO conteudo do numero: %d", numero);
 
     *ponteiro = 50;
+    *ponteiro2 = 1000;
 
-    printf("\nO conteudo do numero: %d", &ponteiro);
+    printf("\nO conteudo do numero: %d", numero);
     printf("\nO endereco do numero (atraves do ponteiro): %d", *ponteiro);
 
     return 0;
