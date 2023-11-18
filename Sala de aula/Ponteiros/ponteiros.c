9507a65bb26ce2e94b9661a1b2da7b366e052b97
AlineOP201 <alineveira2@gmail.com>
2023-11-18 10:05:51 -0300

    10h05

--- Sala de aula/Ponteiros/ponteiros.c
+++ Sala de aula/Ponteiros/ponteiros.c
@@ -6,13 +6,29 @@
 */
 #include <stdio.h>
 
-int main () {
+int main () 
+{
 // Variável
     int numero;
     int *ponteiro;
+    // Declaração de ponteiros:
+    /*  
+        float *p2;
+        double *p3;
+        struct pessoa *p;
+    */
 
-// Entrada de Dados
-    printf("O endereco da variavel 'numero': %d", &numero);
+    // Atribuição de valores as variaveis
+    numero = 10;
+    ponteiro = &numero;
+
+    printf("O endereco de memoria do numero: %p", &numero);
+    printf("\nO endereco de memoria do ponteiro: %p", &ponteiro);
+
+    // Visualizar o conteúdo
+    printf("\nO conteudo do numero: %d", numero);
+    printf("\nO conteudo do ponteiro: %p", ponteiro);   //Em hexadecimal
+
 
     return 0;
 }
