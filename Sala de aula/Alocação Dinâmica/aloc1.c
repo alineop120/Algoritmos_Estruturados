63ee62641a3e7b69f397c07cdf6f9d8ae24c4880
AlineOP201 <alineveira2@gmail.com>
2023-11-22 10:07:11 -0300

    10h07

--- Sala de aula/Alocação Dinâmica/aloc1.c
+++ Sala de aula/Alocação Dinâmica/aloc1.c
@@ -6,12 +6,15 @@ int main()
     //Alocação dinâmica de mémoria
         int *ponteiro;
         int numero; //Alocação Estática
+        char *letra;
 
         ponteiro = (int*)malloc(sizeof(int));
+        letra = (char*)malloc(sizeof(char));
 
         if (ponteiro == NULL)
         {
             printf("Sem memoria suficinte.");
+            return;
         }
 
         printf("Conteudo do ponteiro: %p.", ponteiro);
@@ -19,8 +22,12 @@ int main()
     
     // Inserção de Dados no espaço de memoria alocado
         *ponteiro = 10;
+        *letra = 'A';
 
         printf("\nConteudo armazenado no endereco de memoria alocado: %d.", *ponteiro);
+        printf("\nConteudo armazenado no endereco de memoria alocado: %c.", *letra);
 
+    free(ponteiro);
+    free(letra);
     return 0;
 }
