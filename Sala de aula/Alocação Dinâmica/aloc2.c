a36dca70a018b9ba129015b574ea789c365c9549
AlineOP201 <alineveira2@gmail.com>
2023-11-22 10:21:48 -0300

    10h21

--- Sala de aula/Alocação Dinâmica/aloc2.c
+++ Sala de aula/Alocação Dinâmica/aloc2.c
@@ -1,3 +1,6 @@
+/*
+    Alocação com Vetor
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,7 +11,26 @@ int main()
 
 
         vetor = (int*)malloc(5 * sizeof(int));
+        for (int i = 0; i < 5; i++)
+        {
+            vetor[i] = i + 1;
+        }
+    // Realocar memoria para aumentar o vetor
+        vetor = (int*)realloc(vetor, 3 * sizeof(int));
+        for (int i = 0; i < 8; i++)
+        {
+            printf("%d\n", vetor[i]);
+        }
+        for (int i = 0; i < 8; i++)
+        {
+            vetor[i] = i + 1;
+        }
+        for (int i = 0; i < 8; i++)
+        {
+            printf("%d\n", vetor[i]);
+        }
         
+        free(vetor); 
 
     return 0;
 }
