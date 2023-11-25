15bf60c863cb86d79e2bd65a8d7536591ac3032d
AlineOP201 <alineveira2@gmail.com>
2023-11-25 09:52:40 -0300

    09h52 montando uma lista

--- 01 Sala de aula/Lista/lista.c
+++ 01 Sala de aula/Lista/lista.c
@@ -1,2 +1,57 @@
+/*
+    Lista Rncadeada
+*/
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+typedef struct Pessoa
+{
+    int id;
+    char nome[30];
+    int idade;
+    struct Pessoa *prox;
+}
+Pessoa;
+
+int main()
+{
+    
+    int opcao;
+
+    do
+    {
+        printf("Digite um opcao a seguir:");
+        printf("1 - cadastrar");
+        printf("2 - mostrar");
+        printf("3 - buscar um registro");
+        printf("4 - alterar um registro");
+        printf("5 - excluir um registro");
+        printf("0 - sair do programa");
+    
+        switch (opcao)
+        {
+        case 1:
+            
+            break;
+        case 2:
+            
+            break;
+        case 3:
+            
+            break;
+        case 4:
+            
+            break;
+        case 5:
+            
+            break;
+        default:
+            break;
+        }
+
+    } while (opcao != 0);
+    
+
+    return 0;
+}
